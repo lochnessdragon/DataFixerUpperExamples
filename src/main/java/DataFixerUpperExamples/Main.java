b59b15786b120a6148128c48807008fbd5c2059f
lochnessdragon <someone@somesite.com>
2020-02-11 21:34:45 -0500

    Added more

--- src/main/java/DataFixerUpperExamples/Main.java
+++ src/main/java/DataFixerUpperExamples/Main.java
@@ -1,9 +1,18 @@
 package DataFixerUpperExamples;
 
+import java.util.concurrent.Executors;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.mojang.datafixers.DSL;
+import com.mojang.datafixers.DataFix;
+import com.mojang.datafixers.DataFixUtils;
+import com.mojang.datafixers.DataFixer;
 import com.mojang.datafixers.DataFixerBuilder;
+import com.mojang.datafixers.Dynamic;
+import com.mojang.datafixers.schemas.Schema;
+import com.mojang.datafixers.types.DynamicOps;
 
 public class Main {
 	
@@ -13,7 +22,22 @@ public class Main {
 		logger.info("Main started!");
 		
 		
-		DataFixerBuilder dataFixerBuilder = new DataFixerBuilder(1);
+		
+		DataFixerBuilder dataFixerBuilder = new DataFixerBuilder(DataFixUtils.getVersion(DataFixUtils.makeKey(2)));
+		
+		Schema version1Schema = new Schema(DataFixUtils.makeKey(1), null);
+		
+		
+		logger.info("Adding Schemas");
+		
+		
+		logger.info("Adding data fixes");
+		
+		
+		logger.info("Building Fixer");
+		DataFixer fixer = dataFixerBuilder.build(Executors.newSingleThreadExecutor());
+		
+		
 	}
 
 }
